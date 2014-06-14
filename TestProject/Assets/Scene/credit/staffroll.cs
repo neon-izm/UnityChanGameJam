e2a7beab77eb5d1b3743f6cfe83062be7a5f995e
msx <zhou.xinxin@nagoya-bunri.ac.jp>
2014-06-14 18:18:26 +0900

    add staffroll

---
+++ TestProject/Assets/Scene/credit/staffroll.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class staffroll : MonoBehaviour {
+	
+	public float speed = 1;
+	
+	void Update () {
+		transform.Translate(Vector3.up * speed * Time.deltaTime);
+	}
+
+}
