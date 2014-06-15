14523f1d4efd98f9a24e45d2b64166d622e4db0c
msx <zhou.xinxin@nagoya-bunri.ac.jp>
2014-06-15 13:29:20 +0900

    add staff-roll

---
+++ TestProject/Assets/Scene/credit/LoadScene.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoadScene : MonoBehaviour {
+
+	void Replay(){
+		Application.LoadLevel("TitleScene");
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
